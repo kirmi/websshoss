389ba03f4bd90c837e96b1f1915f39dcb7dd0fa5
javaLDY <1039236687@qq.com>
2014-12-26 17:05:51 +0800

    啊啊

--- web/js/login.js
+++ web/js/login.js
@@ -2,13 +2,26 @@ var ss = $.noConflict();
 ss(document).ready(function(){
     ss("#UserName").blur(function(){
        if((ss("#UserName").val()==="")||(ss("#UserName").val().length<1)){
-           ss("#id1").text("用户名不能为空");
+           ss("#id1").text("邮箱不能为空");
            return;
         }
-       if((ss("#UserName").val()!=="")&&(!ss("#UserName").val().match(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+ss/))){
+       if((!ss("#UserName").val().match(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/))){
             ss("#id1").text("邮箱不符合规范");
             return;
         }else{
+           ss.ajax({
+                type : "POST",
+                url : _path+"/shinowit/loginemail",
+                data : {valid:ss("#UserName").val()},
+                dataType : "Text",
+                success : function(msg){
+                    ss("#id1").text(msg);
+                },
+                error : function(msg){
+                    ss("#id1").text(msg);
+                }
+           })
+           ss("#UserName")
            ss("#id1").text("");
        }
     });
