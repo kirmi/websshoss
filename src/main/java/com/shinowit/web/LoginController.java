ebc12113d0d578a44328cacb4511d50f2f8092b8
javaLDY <1039236687@qq.com>
2014-12-26 17:05:25 +0800

    啊啊

--- src/main/java/com/shinowit/web/LoginController.java
+++ src/main/java/com/shinowit/web/LoginController.java
@@ -2,11 +2,16 @@ package com.shinowit.web;
 
 import com.shinowit.dao.mapper.ToolsDao;
 import com.shinowit.dao.mapper.TbaMemberinfoMapper;
+import com.shinowit.entity.TbaMemberinfo;
+import com.shinowit.entity.TbaMemberinfoCriteria;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
@@ -20,25 +25,43 @@ import java.util.Map;
 public class LoginController {
 
     @Resource
-    private TbaMemberinfoMapper memberinfoMapper;
+    private TbaMemberinfoMapper memberdao;
 
     @Resource
     private ToolsDao dao;
-
-    @RequestMapping("/login")
-    public String loginshow(){
-        return "/login";
-    }
 
     @RequestMapping("/loginemail")
     public void testemail(@RequestParam("valid") String emailvalue,HttpServletResponse response){
             List<Map<String,Object>> memberlist = dao.selectstatus(emailvalue);
             if(memberlist.size()<1){
                 try {
-                    response.getWriter().println("邮箱不存在或者还没有激活!");
+                    response.getWriter().println("邮箱没有注册或者还没有激活!");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
+    }
+    @RequestMapping("/validlogin")
+    public String testlogin(@ModelAttribute("loginmem")TbaMemberinfo memebr,BindingResult bindingResult,HttpServletRequest request){
+        if(bindingResult.hasErrors()){
+            return "/login";
+        }
+        if((memebr.getEmail()==null)||(memebr.getEmail().trim().length()<1)){
+            request.setAttribute("username","用户名不能为空");
+            return "/login";
+        }
+        if((memebr.getPwd()==null)||(memebr.getPwd().length()<1)){
+            request.setAttribute("username","密码不能为空");
+            return "/login";
+        }
+        TbaMemberinfoCriteria criteria = new TbaMemberinfoCriteria();
+        TbaMemberinfoCriteria.Criteria tj = criteria.createCriteria();
+        tj.andEmailEqualTo(memebr.getEmail());
+        List<TbaMemberinfo> memberlist = memberdao.selectByExample(criteria);
+        if(memberlist.size()>0){
+            return "/index";
+        }else{
+            return "/login";
+        }
     }
 }
